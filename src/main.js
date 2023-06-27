91760f68387ba8f0c845b9cbb4f48c7031a51968
Vlad Stawizki <vlad.stawizki@gmail.com>
2023-06-27 11:52:01 +0200

    added main page with links

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { LitElement, html } from 'lit'
 
 import { ProgressBar } from './progressbar'
 import { Parallax } from './parallax'
+import { Home } from './home'
 
 import './css/style.css'
 
@@ -17,11 +18,14 @@ class App extends LitElement {
   }
 
   render() {
+    console.log(this.pathname);
     switch (this.pathname) {
+      case '/':
+        return html`<my-home></my-home>`;
       case '/progressbar':
         return html`<my-progressbar></my-progressbar>`;
       case '/parallax':
-        return html`<my-parallax></my-paralax>`;
+        return html`<my-parallax></my-parallax>`;
       default:
         return html`<h1>oO 404</h1>`
     }
@@ -31,3 +35,4 @@ class App extends LitElement {
 customElements.define('my-app', App);
 customElements.define('my-progressbar', ProgressBar);
 customElements.define('my-parallax', Parallax);
+customElements.define('my-home', Home);
